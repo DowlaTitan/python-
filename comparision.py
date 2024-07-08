8efc3a31ab175e7954322c50a77791b4406a8da2
DowlaTitan <dowlatitan@gmail.com>
2024-07-08 18:09:39 +0530

    2nd

---
+++ comparision.py
@@ -0,0 +1,12 @@
+a=78
+b=79
+if(a>b):
+    print("a is big",a)
+else:
+    print("b is big",b)
+>
+>=
+<
+<=
+==
+!=
---
+++ comparision1.py
@@ -0,0 +1,22 @@
+a=(input('enter a value'))
+b=(input('enter b value'))
+if (a>b):
+    print("a is big",a)
+else:
+    print("b is big",b)
+if(a<b):
+    print("a is small",a)
+else:
+    print("b is small",b)
+if(a>=b):
+    print("a is big",a)
+else:
+    print("b is big",b)
+if (a<=b):
+    print("a is small",a)
+else:
+    print("b is small",b)
+if(a!=b):
+    print("a is not equal to b")
+else:
+    print("a is equal to b")
